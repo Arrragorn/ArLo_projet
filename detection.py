a439a7aa476cb114d9a88234ba9a55617e23661f
Arrragorn <antoine@ArchAntoine.localdomain>
2020-12-05 16:50:00 -0500

    test fonctionnel pour détection

--- detection.py
+++ detection.py
@@ -8,6 +8,7 @@ Created on Sat Dec  5 14:40:11 2020
 
 import skimage
 import skimage.io
+import skimage.filters
 
 import numpy as np
 
@@ -17,20 +18,39 @@ import matplotlib
     
 def detect(image1,image2):
     
-    diff_image = np.absolute(image1 - image2)
+    diff_image = skimage.img_as_ubyte(np.absolute(skimage.img_as_int(image2) - skimage.img_as_int(image1)))
+    
+    
+    
+    plt.imshow(diff_image)
+    plt.text(0,0,"diff")
+    plt.show()
     
     filterd_image = skimage.filters.median(diff_image,)
+    
+    plt.imshow(filterd_image)
+    plt.text(0,0,"filtered")
+    plt.show()
+    
   
-    thresh_image = skimage.filters.apply_hysteresis_threshold(filterd_image, 4, 5)
-    return thresh_image
+    thresh_image = skimage.filters.apply_hysteresis_threshold(filterd_image, 254, 30)
+    
+    image_et_thresh = image2
+    for i in range(len(thresh_image)):
+        for j in range(len(thresh_image[0])):
+            if(thresh_image[i][j][0]):
+                image_et_thresh[i][j] = [255,255,255]
+        
+    return image_et_thresh
 
 
 if (__name__ == "__main__"):
-    image1= skimage.io.imread("gandalf-lord-of-the-rings-e1534255368438.jpg")
-    image2 = skimage.io.imread("Inkedgandalf-lord-of-the-rings-e1534255368438_LI (2).jpg")
+    image1 = skimage.io.imread("Inkedgandalf-lord-of-the-rings-e1534255368438_LI (2).jpg")
+    image2 = skimage.io.imread("gandalf2.jpg")
     
-    plt.imshow(image1)
-    plt.imshow(image2)
-    #test = detect(image1,image2)
+
+    test = detect(image1,image2)
     
-    #plt.imshow(test)
+    plt.imshow(test)
+    plt.text(0,0,"mouv")
+    plt.show()
